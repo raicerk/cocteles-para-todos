6cfcd47a421139792378d3700907a06e77bbfa66
Juan Mora <jmora@serv.externos.prismamp.com>
2023-05-01 17:29:28 -0400

    feat: add new config

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,20 +4,28 @@ import { AppService } from './app.service';
 import { IngredientesModule } from './ingredientes/ingredientes.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Ingrediente } from './ingredientes/entities/ingrediente.entity';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
-    IngredientesModule,
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: 'cocteles',
-      database: 'cocteles-para-todos',
-      entities: [Ingrediente],
-      synchronize: true,
+    ConfigModule.forRoot({
+      isGlobal: true,
     }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (config: ConfigService) => ({
+        type: 'mysql',
+        host: config.get<string>('DATABASE_HOST'),
+        port: parseInt(config.get<string>('DATABASE_PORT')) || 3306,
+        username: config.get<string>('DATABASE_USER'),
+        password: config.get<string>('DATABASE_PASSWORD'),
+        database: config.get<string>('DATABASE_BASE'),
+        entities: [Ingrediente],
+        synchronize: true,
+      }),
+      inject: [ConfigService],
+    }),
+    IngredientesModule,
   ],
   controllers: [AppController],
   providers: [AppService],
