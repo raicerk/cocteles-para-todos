6620bb5009406b68ce67e47e1371a4fd81c4944d
Juan Mora <jmora@serv.externos.prismamp.com>
2023-06-04 12:42:58 -0500

    feat: update documentation for swagger of receipts

--- src/receipts/receipts.controller.ts
+++ src/receipts/receipts.controller.ts
@@ -6,21 +6,32 @@ import {
   Patch,
   Param,
   Delete,
+  HttpCode,
+  HttpStatus,
 } from '@nestjs/common';
 import { ReceiptsService } from './receipts.service';
 import { CreateReceiptDto } from './dto/create-receipt.dto';
 import { UpdateReceiptDto } from './dto/update-receipt.dto';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { Receipt, ReceiptCreate } from './entities/receipt.entity';
 
+@ApiTags('receipts')
 @Controller('receipts')
 export class ReceiptsController {
   constructor(private readonly receiptsService: ReceiptsService) {}
 
   @Post()
+  @HttpCode(HttpStatus.OK)
+  @ApiOperation({ summary: 'Insert a new receipt' })
+  @ApiResponse({ type: ReceiptCreate, status: HttpStatus.OK })
   create(@Body() createReceiptDto: CreateReceiptDto) {
     return this.receiptsService.create(createReceiptDto);
   }
 
   @Get()
+  @HttpCode(HttpStatus.OK)
+  @ApiOperation({ summary: 'Get all receipts' })
+  @ApiResponse({ type: Receipt, status: HttpStatus.OK, isArray: true })
   findAll() {
     return this.receiptsService.findAll();
   }
