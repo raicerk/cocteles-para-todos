c956adba6aef127d774a026f7d0a9920df9af62d
Juan Mora <jmora@serv.externos.prismamp.com>
2023-06-04 12:29:11 -0500

    feat: update dto and entity for receipts

--- src/receipts/dto/create-receipt.dto.ts
+++ src/receipts/dto/create-receipt.dto.ts
@@ -1,1 +1,37 @@
-export class CreateReceiptDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import {
+  IsBoolean,
+  IsDate,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+} from 'class-validator';
+
+export class CreateReceiptDto {
+  @ApiProperty({ description: 'Name of receipt' })
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @ApiProperty({ description: 'Author to receipt' })
+  @IsString()
+  @IsNotEmpty()
+  author: string;
+
+  @ApiProperty({ description: 'Datetime to create receipt' })
+  @IsDate()
+  datetime: Date;
+
+  @ApiProperty({ description: 'History of receipts' })
+  @IsString()
+  @IsOptional()
+  history: string;
+
+  @ApiProperty({ description: 'Preparation of receipt' })
+  @IsString()
+  preparation: string;
+
+  @ApiProperty({ description: 'Status of publication of receipt' })
+  @IsBoolean()
+  public: boolean;
+}
--- src/receipts/entities/receipt.entity.ts
+++ src/receipts/entities/receipt.entity.ts
@@ -1,1 +1,38 @@
-export class Receipt {}
+import { ApiProperty } from '@nestjs/swagger';
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Receipt {
+  @ApiProperty({ description: 'Receipt Id' })
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @ApiProperty({ description: 'Name of receipt' })
+  @Column()
+  name: string;
+
+  @ApiProperty({ description: 'Author to receipt' })
+  @Column()
+  author: string;
+
+  @ApiProperty({ description: 'Datetime to create receipt' })
+  @Column({ default: new Date() })
+  datetime: Date;
+
+  @ApiProperty({ description: 'History of receipts' })
+  @Column()
+  history: string;
+
+  @ApiProperty({ description: 'Preparation of receipt' })
+  @Column()
+  preparation: string;
+
+  @ApiProperty({ description: 'Status of publication of receipt' })
+  @Column({ default: true })
+  public: boolean;
+}
+
+export class ReceiptCreate {
+  @ApiProperty({ description: 'Receipt Id' })
+  id: number;
+}
